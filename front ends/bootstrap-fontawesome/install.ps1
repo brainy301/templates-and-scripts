415a8ed79068e24961e7875feb33efecc2d36176
brainy301 <r.becker.brainlab@gmail.com>
2018-04-29 18:17:04 +0200

    commit

--- front ends/bootstrap-fontawesome/install.ps1
+++ front ends/bootstrap-fontawesome/install.ps1
@@ -12,13 +12,14 @@
 #	git config credential.helper store
 #	git push origin master
 #	git pull
+#	dotnet new webapi -lang C#
 
 # Config:
 $FolderName = (Get-Item -Path ".\").Name
 $FolderPath = (Get-Item -Path ".\").FullName
 
 # Create bower file
-$ProjectName = $FolderName.ToLower()
+<#$ProjectName = $FolderName.ToLower()
 $Content = "
 { 
 	""name"": ""$($ProjectName)"", 
@@ -42,26 +43,30 @@ Write-Output 'bower file created'
 # Install dependencies
 Write-Output 'installing fontawesome...'
 bower install fontawesome#4.7.0 --save
-Write-Output 'fontawesome installed...'
+Write-Output 'fontawesome installed.'
 
 Write-Output 'installing bootstrap...'
 bower install bootstrap#4.1.0
-Write-Output 'bootstrap installed...'
+Write-Output 'bootstrap installed.'
 
 Write-Output 'installing jquery...'
 bower install jquery#3.3.1
-Write-Output 'jquery installed...'
+Write-Output 'jquery installed.'
 
 Write-Output 'downloading site icon...'
-Invoke-WebRequest -Uri "http://www.iconeasy.com/icon/ico/Application/iPhonica%20Vol.%202/Notes.ico" -OutFile ("$FolderPath\images\notes.ico")
+(New-Object System.Net.WebClient).DownloadFile("http://www.iconeasy.com/icon/ico/Application/iPhonica%20Vol.%202/Notes.ico", ("$FolderPath\images\favicon.ico"))
+Write-Output 'site icon downloaded.'
+#>
+Write-Output 'downloading default.html...'
+Write-Output $("$FolderPath\default.html")
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/brainy301/templates-and-scripts/master/front%20ends/bootstrap-fontawesome/default.html", $("$FolderPath\default.html"))
+Write-Output 'defualt.html downloaded.'
 
-Invoke-WebRequest -Uri "http://www.iconeasy.com/icon/ico/Application/iPhonica%20Vol.%202/Notes.ico" -OutFile "D:\#\Kirk\Desktop\Notebook\Notebook.Frontend\images\notes.ico"
-(New-Object System.Net.WebClient).DownloadFile("http://www.iconeasy.com/icon/ico/Application/iPhonica%20Vol.%202/Notes.ico", "D:\#\Kirk\Desktop\Notebook\Notebook.Frontend\images\notes.ico")
+
+
+
 
 # Pause to check
 Write-Host -NoNewLine 'Press any key to continue...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 #Read-Host -Prompt 'Press any key to continue'
-
-
-dotnet new webapi -lang C#
